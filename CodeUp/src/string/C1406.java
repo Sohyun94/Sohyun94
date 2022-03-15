c340268fdc42d20521b27129e49b503746d380c2
Sohyun94 <garamsh@gmail.com>
2022-03-16 02:31:19 +0900

    22-03-16 02:31

---
+++ CodeUp/src/string/C1406.java
@@ -0,0 +1,17 @@
+package string;
+
+import java.util.Scanner;
+
+public class C1406 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		if (s.equals("love"))
+			System.out.println("I love you.");
+		else
+			System.out.println();
+
+	}
+
+}
