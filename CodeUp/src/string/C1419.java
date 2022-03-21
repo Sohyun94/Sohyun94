c44f85a648b0d93f22818510fc9a13df80bb8e28
Kim <garamsh@gmail.com>
2022-03-21 19:58:20 +0900

    22-03-21 19:58

--- CodeUp/src/string/C1419.java
+++ CodeUp/src/string/C1419.java
@@ -6,11 +6,12 @@ public class C1419 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String love[] = sc.next().split("");
+		String love[] = sc.nextLine().split("");
 		int cnt = 0;
-		for(int i= 0; i < love.length; i++) {
-			if(love[i].equals("l") && love[i+1].equals("o") && love[i+2].equals("v") && love[i+3].equals("e"))
+		for(int i= 0; i < love.length - 3; i++) {
+			if(love[i].equals("l") && love[i+1].equals("o") && love[i+2].equals("v") && love[i+3].equals("e")) {
 				cnt++;
+			}
 		}
 		System.out.println(cnt);
 	}
---
+++ CodeUp/src/string/C1733.java
@@ -0,0 +1,18 @@
+package string;
+
+import java.util.Scanner;
+
+public class C1733 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		if(s.equals("IOI")) {
+			System.out.println("IOI is the International Olympiad in Informatics.");
+		} else {
+			System.out.println("I don't care.");
+		}
+
+	}
+
+}
