ed1552d885697fd5bff501eb45429aa54b595859
Kim <garamsh@gmail.com>
2022-03-31 16:47:08 +0900

    22-03-31 16:47

---
+++ Baekjoon/src/_220331/B1966_프린터큐.java
@@ -0,0 +1,47 @@
+package _220331;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class B1966_프린터큐 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt(); // 테스트 케이스 수
+		for (int tc = 1; tc <= T; tc++) {
+			int num = sc.nextInt(); // 문서 개수
+			int pos = sc.nextInt(); // 궁금한 문서 현재 위치
+			int idx = 0; // 궁금한 문서 인쇄 순서
+
+			Queue<int[]> printer = new LinkedList<>(); // 프린터 큐
+
+			for (int i = 0; i < num; i++) { // 문서들의 현재 위치와 중요도 받기
+				printer.add(new int[] { i, sc.nextInt() });
+			} // 입력 끝
+
+			while (true) {
+				int[] first = printer.poll(); // 큐의 첫 번째 원소 프린터에서 꺼내기
+				boolean check = true; // 가장 중요도가 큰지 판별할 boolean
+
+				for (int p[] : printer) { // 프린터 큐 내에
+					if (first[1] < p[1]) {// 첫 번째 원소보다 중요도가 큰 애가 있다면
+						check = false; // false로 바꾸고
+						break; // 동작 종료
+					}
+				} // 확인 for문
+				if (check == true) {
+					idx++; // check가 true면 idx에 1 더해 주기 (인쇄 중이니까)
+					if (first[0] == pos) // 그러다가 내가 궁금한 문서 차례 되면 멈추기
+						break;
+				} else {
+					printer.add(first); // 아니면 프린터에 다시 넣기
+				}
+
+			} // 인쇄 과정 끝
+			System.out.println(idx);
+		} // tc
+
+	}// main
+
+}
--- SWEA/src/swea1232_사칙연산.java
+++ SWEA/src/swea1232_사칙연산.java
@@ -1,36 +1,45 @@
 import java.util.Scanner;
 
 public class swea1232_사칙연산 {
-	static String[][] tree;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		for (int tc = 1; tc <= 10; tc++) {
-			int N = sc.nextInt(); // 정점 수
+			int node = sc.nextInt(); // 정점 수
+			char[] op = new char[node + 1]; // 연산자 담을 배열
+			int[] childL = new int[node + 1]; // 왼쪽 자식
+			int[] childR = new int[node + 1]; // 오른쪽 자식
+			int[] ans = new int[node + 1]; // 연산 결과 배열
 
-			tree = new String[N + 1][]; // 노드 1번부터 하기 위해 +1
+			int idx = 0; // 위치 체크
+			int end = 0; // 마지막 노드 체크
+
 			sc.nextLine(); // 캐리지 리턴 버리기
-			for (int i = 1; i <= N; i++) {
-				tree[i] = sc.nextLine().split(" "); // 공백 포함 한 줄로 받으려고
-			} // 입력 끝
-			
-			System.out.println("#" + tc + " " + (int)op(1));
 
-		} // tc
-
-	}// main
-
-	private static double op(int idx) {
-		if(tree[idx][1].equals("+")) //더하기
-			return op(Integer.parseInt(tree[idx][2])) + op(Integer.parseInt(tree[idx][3]));
-		else if(tree[idx][1].equals("-")) //빼기
-			return op(Integer.parseInt(tree[idx][2])) - op(Integer.parseInt(tree[idx][3]));
-		else if(tree[idx][1].equals("*")) //곱하기
-			return op(Integer.parseInt(tree[idx][2])) * op(Integer.parseInt(tree[idx][3]));
-		else if(tree[idx][1].equals("/")) //나누기
-			return op(Integer.parseInt(tree[idx][2])) / op(Integer.parseInt(tree[idx][3]));
-		else //숫자일 때
-			return Double.parseDouble(tree[idx][1]); //묵시적 형변환 때문에 integer로 해도 상관 X
-	}
-
+			for (int i = 1; i <= node; i++) {
+				String[] math = sc.nextLine().split(" "); // 문자열 배열에 공백 자르고 하나씩 담기
+				idx = Integer.parseInt(math[0]); // 위치 배열 첫 번째로
+				if (math.length == 4) { // 길이 4 되면
+					op[idx] = math[1].charAt(0); // 연산자 담기
+					childL[idx] = Integer.parseInt(math[2]); // 왼쪽 자식 담기
+					childR[idx] = Integer.parseInt(math[3]); // 오른쪽 자식 담기
+					end = idx; // end를 마지막 인덱스로
+				} else {
+					ans[idx] = Integer.parseInt(math[1]);
+				}
+			} // 정렬 끝
+			for (int i = end; i >= 1; i--) {
+				if (op[i] == '+') {
+					ans[i] = ans[childL[i]] + ans[childR[i]];
+				} else if (op[i] == '-') {
+					ans[i] = ans[childL[i]] - ans[childR[i]];
+				} else if (op[i] == '*') {
+					ans[i] = ans[childL[i]] * ans[childR[i]];
+				} else if (op[i] == '/') {
+					ans[i] = ans[childL[i]] / ans[childR[i]];
+				}
+			}// 계산 끝
+				System.out.println("#" + tc + " " + ans[1]);
+		}//tc 
+	}//main
 }
