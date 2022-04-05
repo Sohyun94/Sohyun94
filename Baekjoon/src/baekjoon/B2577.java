1b284043ea88cd75de4d743483bbe64ed87dbab8
Kim Sohyun <garamsh@gmail.com>
2022-04-06 00:17:32 +0900

    22-04-06 00:17

--- Baekjoon/src/baekjoon/B2577.java
+++ Baekjoon/src/baekjoon/B2577.java
@@ -1,10 +1,26 @@
 package baekjoon;
 
+import java.util.Scanner;
+
 public class B2577 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		int num = A * B * C;
+		String number = Integer.toString(num);
+		
+		for(int i = 0; i <= 9; i++) {
+			int cnt = 0;
+			for(int j = 0; j < number.length(); j++) {
+				if((number.charAt(j) - '0') == i) {
+					cnt++;
+				}
+			}
+			System.out.println(cnt);
+		}
 	}
 
 }
