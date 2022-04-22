ab63483686d88db64b8d69c3ead3c3b53e476a4f
Kim Sohyun <garamsh@gmail.com>
2022-04-22 23:53:57 +0900

    22-04-22 23:53

---
+++ Baekjoon/src/class1/B1000_A더하기B.java
@@ -0,0 +1,15 @@
+package class1;
+
+import java.util.Scanner;
+
+public class B1000_A더하기B {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println(a + b);
+
+	}
+
+}
