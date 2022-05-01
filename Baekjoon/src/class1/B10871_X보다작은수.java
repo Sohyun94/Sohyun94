b05ba8e14c28cae2766a6e41c87cb4a7d510b404
Kim Sohyun <garamsh@gmail.com>
2022-05-02 03:58:45 +0900

    22-05-02 03:53

---
+++ Baekjoon/src/class1/B10871_X보다작은수.java
@@ -0,0 +1,22 @@
+package class1;
+
+import java.util.Scanner;
+
+public class B10871_X보다작은수 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int X = sc.nextInt();
+		int[] arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		for (int i = 0; i < N; i++) {
+			if (arr[i] < X)
+				System.out.print(arr[i] + " ");
+		}
+
+	}
+
+}
