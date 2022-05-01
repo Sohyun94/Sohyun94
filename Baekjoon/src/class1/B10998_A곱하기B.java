a000a7a5f3264507f746dcca1d45159b1dafdc33
Kim Sohyun <garamsh@gmail.com>
2022-05-01 23:20:18 +0900

    22-05-01 23:20

---
+++ Baekjoon/src/class1/B10998_A곱하기B.java
@@ -0,0 +1,14 @@
+package class1;
+
+import java.util.Scanner;
+
+public class B10998_A곱하기B {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		System.out.println(A * B);
+	}
+
+}
