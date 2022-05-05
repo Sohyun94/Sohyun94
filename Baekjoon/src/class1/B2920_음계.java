2011e486cb26fc09512a32b3c6291ab3064b64ae
Kim Sohyun <garamsh@gmail.com>
2022-05-05 23:56:47 +0900

    22-05-05 23:56

---
+++ Baekjoon/src/class1/B2920_음계.java
@@ -0,0 +1,26 @@
+package class1;
+
+import java.util.Scanner;
+
+public class B2920_음계 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] arr = new int[8];
+		String result = "";
+		for (int i = 0; i < 8; i++) {
+			arr[i] = sc.nextInt();
+		}
+		for (int i = 0; i < 7; i++) {
+			if (arr[i] == arr[i + 1] - 1) {
+				result = "ascending";
+			} else if (arr[i] == arr[i + 1] + 1) {
+				result = "descending";
+			} else {
+				result = "mixed";
+				break;
+			}
+		}
+		System.out.println(result);
+	}
+}
