a3ce1331fd22d28ddb394955d417981836d0c1dc
Kim Sohyun <garamsh@gmail.com>
2022-05-09 23:23:30 +0900

    22-05-09 23:23

---
+++ Baekjoon/src/class2/B1018_체스판다시칠하기.java
@@ -0,0 +1,15 @@
+package class2;
+
+import java.util.Scanner;
+
+public class B1018_체스판다시칠하기 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt(); // 세로
+		int M = sc.nextInt(); // 가로
+		int[][] board = new int[N][M];
+
+	}
+
+}
