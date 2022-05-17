c0e630d04fc846045252f9b2c11b09e42bbe732b
Kim Sohyun <garamsh@gmail.com>
2022-05-17 23:57:42 +0900

    22-05-17 23:57

---
+++ Baekjoon/src/class1/B1001_A빼기B.java
@@ -0,0 +1,15 @@
+package class1;
+
+import java.util.Scanner;
+
+public class B1001_A빼기B {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		System.out.println(A-B);
+
+	}
+
+}
