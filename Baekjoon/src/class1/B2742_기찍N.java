9c19cef4d10ffe96a27df6e528888bf36fd6fbe7
Kim Sohyun <garamsh@gmail.com>
2022-05-27 18:41:36 +0900

    22-05-27 18:41

---
+++ Baekjoon/src/class1/B2742_기찍N.java
@@ -0,0 +1,14 @@
+package class1;
+
+import java.util.Scanner;
+
+public class B2742_기찍N {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		for(int i = N ; i > 0; i--)
+			System.out.println(i);
+	}
+
+}
