57918b40fd942d40ab7ff0c0a964e19ff77a275d
Kim Sohyun <janeqcitizen@naver.com>
2022-06-16 23:47:14 +0900

    22-06-16 23:47

---
+++ Baekjoon/src/baekjoon/B11382.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B11382 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double A = sc.nextDouble();
+		double B = sc.nextDouble();
+		double C = sc.nextDouble();
+		double ans = A + B + C;
+		System.out.printf("%.0f", ans);
+
+	}
+
+}
