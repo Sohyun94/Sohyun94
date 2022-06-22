fa99e54b54251886dd97b9ee4dd540c1a290a12e
Kim Sohyun <janeqcitizen@naver.com>
2022-06-22 20:53:01 +0900

    22-06-22 20:52

---
+++ Baekjoon/src/june21st/B1193_분수찾기.java
@@ -0,0 +1,32 @@
+package june21st;
+
+import java.util.Scanner;
+
+public class B1193_분수찾기 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int[][] idx = new int[x/2][x/2];
+		String[][] arr = new String[x][x];
+		int cnt = 0;
+		
+		for(int i = 0; i < x/2; i++) {
+			for(int j = 0; j< x/2; j++) {
+				
+			}
+		}
+
+//		for (int i = 0; i < x; i++) {
+//			for (int j = 0; j < x; j++) {
+//				arr[i][j] = (i+1) + "/" +(j+1);
+//			}
+//		} // 입력 완료
+//		for(int i = 0; i < 6; i++) {
+//			for(int j = 0; j < 6 ; j++)
+//				System.out.print(arr[i][j] + " ");
+//		}
+//		System.out.println();
+	}
+
+}
