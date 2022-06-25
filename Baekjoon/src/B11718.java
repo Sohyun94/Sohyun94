e042ec1df82d58b761da4a7e9ec31f0916ee4999
Kim Sohyun <janeqcitizen@naver.com>
2022-06-25 23:55:17 +0900

    22-06-25 23:55

---
+++ Baekjoon/src/B11718.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class B11718 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while(sc.hasNext() != false) {
+		String s = sc.nextLine();
+		System.out.println(s);
+		}
+	}
+
+}
