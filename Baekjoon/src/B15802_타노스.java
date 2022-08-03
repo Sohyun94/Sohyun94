aa2445a7ed97aa66f78157efd702c9bfc7860502
Kim Sohyun <janeqcitizen@naver.com>
2022-08-03 21:31:51 +0900

    타노스

---
+++ Baekjoon/src/B15802_타노스.java
@@ -0,0 +1,7 @@
+public class B15802_타노스 {
+
+	public static void main(String[] args) {
+		System.out.println("1");
+		}
+	}
+
---
+++ Baekjoon/src/B15802_타노스2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class B15802_타노스2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		if(str.equals("내 아이디")) {
+			System.out.println(1);
+		} else {
+			System.out.println(0);
+		}
+	}
+
+}
