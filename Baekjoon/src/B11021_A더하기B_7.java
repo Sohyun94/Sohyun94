c67591dcb311e67a9d854bae88210ca5ec2e1548
Kim Sohyun <janeqcitizen@naver.com>
2022-08-09 23:59:18 +0900

    22-8-9 23:59

---
+++ Baekjoon/src/B11021_A더하기B_7.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class B11021_A더하기B_7 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int tc = 0 ; tc < T; tc++) {
+			int result = sc.nextInt() + sc.nextInt();
+			System.out.println("Case " + (tc+1) + ": " + result);
+		}
+
+	}
+
+}
