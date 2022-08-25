aeaa13f42e967e7afb3e33baca162e7faa41d9ce
Kim Sohyun <janeqcitizen@naver.com>
2022-08-25 22:49:45 +0900

    *****

---
+++ Baekjoon/src/B2440.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class B2440 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		for (int i = 0; i < N; i++) {
+			for (int j = N - i - 1; j >= 0; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
