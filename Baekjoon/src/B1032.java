8b236e974d934708b78755b0603b8de97df88c84
SsoHhyun <janeqcitizen@naver.com>
2022-09-12 23:58:04 +0900

    1 sol

---
+++ Baekjoon/src/B1032.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class B1032 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] arr = new int[N];
+
+	}
+
+}
