68a11c2a7cd8f2e8c61018311059e1a25b74f49b
SsoHhyun <janeqcitizen@naver.com>
2022-12-07 23:04:11 +0900

    December 7th, Algo study daily 1 sol

---
+++ StudyAlgo/src/Baekjoon1406.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+public class Baekjoon1406 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		String str = br.readLine();
+		int m = Integer.parseInt(br.readLine());
+
+		LinkedList<Character> list = new LinkedList<>();
+
+		for (int i = 0; i < str.length(); i++) {
+			list.add(str.charAt(i));
+		}
+
+		ListIterator<Character> iter = list.listIterator();
+
+		while (iter.hasNext())
+			iter.next();
+
+		for (int i = 0; i < m; i++) {
+			String cmd = br.readLine();
+			char c = cmd.charAt(0);
+
+			switch (c) {
+			case 'L':
+				if (iter.hasPrevious())
+					iter.previous();
+				break;
+
+			case 'D':
+				if (iter.hasNext())
+					iter.next();
+				break;
+
+			case 'B':
+				if (iter.hasPrevious()) {
+					iter.previous();
+					iter.remove();
+				}
+				break;
+
+			case 'P':
+				char newC = cmd.charAt(2);
+				iter.add(newC);
+				break;
+
+			default:
+				break;
+			}
+		}
+		for (Character ch : list) {
+			bw.write(ch);
+		}
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+
+}
