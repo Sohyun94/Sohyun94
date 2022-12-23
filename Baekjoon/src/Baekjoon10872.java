5047cbcd2e8be96998fd5268896ddd544378697e
SsoHhyun <janeqcitizen@naver.com>
2022-12-23 23:48:16 +0900

    December 23rd, algo study daily 1 sol

---
+++ Baekjoon/src/Baekjoon10872.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon10872 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int ans = factorial(n);
+
+	}
+
+	private static int factorial(int n) {
+		if (n <= 1)
+			return 1;
+		return n * factorial(n - 1);
+	}
+
+}
